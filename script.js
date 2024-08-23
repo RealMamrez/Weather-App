2c22428e5663c6a2c0a4ce883505f694c8b40d1a
Mohammad Razmpour <themamrez.dev@gmail.com>
2024-08-23 04:10:48 +0330

    adding some **** code 🤬

--- script.js
+++ script.js
@@ -139,3 +139,74 @@ search.addEventListener('click', () => {
     });
 
 });
+
+
+let names = [
+    "Ayla",
+    "Jake",
+    "Sean",
+    "Henry",
+    "Brad",
+    "Stephen",
+    "Taylor",
+    "Timmy",
+    "Cathy",
+    "John",
+    "Amanda",
+    "Amara",
+    "Sam",
+    "Sandy",
+    "Danny",
+    "Ellen",
+    "Camille",
+    "Chloe",
+    "Emily",
+    "Nadia",
+    "Mitchell",
+    "Harvey",
+    "Lucy",
+    "Amy",
+    "Glen",
+    "Peter",
+  ];
+  //Sort names in ascending order
+  let sortedNames = names.sort();
+  //reference
+  let input = document.getElementById("searchInput");
+  //Execute function on keyup
+  input.addEventListener("keyup", (e) => {
+    //loop through above array
+    //Initially remove all elements ( so if user erases a letter or adds new letter then clean previous outputs)
+    removeElements();
+    for (let i of sortedNames) {
+      //convert input to lowercase and compare with each string
+      if (
+        i.toLowerCase().startsWith(input.value.toLowerCase()) &&
+        input.value != ""
+      ) {
+        //create li element
+        let listItem = document.createElement("li");
+        //One common class name
+        listItem.classList.add("list-items");
+        listItem.style.cursor = "pointer";
+        listItem.setAttribute("onclick", "displayNames('" + i + "')");
+        //Display matched part in bold
+        let word = "<b>" + i.substr(0, input.value.length) + "</b>";
+        word += i.substr(input.value.length);
+        //display the value in array
+        listItem.innerHTML = word;
+        document.querySelector(".list").appendChild(listItem);
+      }
+    }
+  });
+  function displayNames(value) {
+    input.value = value;
+    removeElements();
+  }
+  function removeElements() {
+    //clear all the item
+    let items = document.querySelectorAll(".list-items");
+    items.forEach((item) => {
+      item.remove();
+    });
+  }
